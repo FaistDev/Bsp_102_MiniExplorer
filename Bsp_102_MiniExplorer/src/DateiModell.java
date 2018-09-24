0160cfd995a388efc4ed998672264ea489fbd866
FaistDev <ben@faistdev.at>
2018-09-24 11:33:12 +0200

    Added NB Project

---
+++ Bsp_102_MiniExplorer/src/DateiModell.java
@@ -0,0 +1,26 @@
+
+import javax.swing.AbstractListModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ben
+ */
+public class DateiModell extends AbstractListModel<Item>{
+
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Item getElementAt(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
